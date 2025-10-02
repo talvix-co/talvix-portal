f77292a004a9b11bd78eee0ac2dd335eed419fd5
Razin <razin.bharde@talvix.co>
2025-10-02 20:49:09 +1300

    fix errors

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { LoginForm } from '../components/auth/LoginForm'
 import { SignUpForm } from '../components/auth/SignUpForm'
 
